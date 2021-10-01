26fc0d82b1cead50a8e3ae0835b31a5e8e06f9b0
GaurabDhakal <dhakalgaurab123@gmail.com>
2021-10-01 15:43:06 +0545

    2.001

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './mycomponents/about/about.component';
 import { ContactComponent } from './mycomponents/contact/contact.component';
 import { HomeComponent } from './mycomponents/home/home.component';
 import { PagenotfoundComponent } from './mycomponents/pagenotfound/pagenotfound.component';
+import { ProjectsComponent } from './mycomponents/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'myprojects', component: ProjectsComponent },
   { path: '**', component: PagenotfoundComponent }
 ];
 
--- src/app/mycomponents/navbar/navbar.component.ts
+++ src/app/mycomponents/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private titlePage:Title){
   }
+  ngOnInit(){}
+  pageTitle(title:string){
+    this.titlePage.setTitle(title)
+  }
+
 
 }
